e2a8030fedcd41a4c09cf6f8833a91c97a790da0
simen <simen@haitech.org>
2017-10-10 09:51:57 +0200

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmM

---
+++ labb/task2/calc.py
@@ -0,0 +1,18 @@
+
+
+fs = 0.1
+rs = 1
+fc = 0.01
+rc = 2
+sw = 0.3
+
+U_s = (fs**2 * (fs * (4*rs**2 + 9*rs*sw + 3*sw**2) + 6*rs**3)) / (3*rs**2*(rs+sw)*(fs*(8*rs+3*sw)+rs*(2*rs+sw)))
+
+U_c = fc / (fc + rc)
+
+
+U = U_s + U_c - U_s * U_c
+
+print "U = {} = {}%".format(U, U*100)
+
+
